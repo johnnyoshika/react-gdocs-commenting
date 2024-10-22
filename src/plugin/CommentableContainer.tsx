9d7788bd9e80085f551989b903b3e9b3c85b46f7
Johnny Oshika <johnnyoshika@gmail.com>
2024-10-22 07:29:46 -0700

    Hoise handling of text selection from CommetableContainer to CommentableSection

--- src/plugin/CommentableContainer.tsx
+++ src/plugin/CommentableContainer.tsx
@@ -1,7 +1,6 @@
 // CommentableContainer.tsx
 import { ReactNode, useEffect, useRef } from 'react';
 import { useSelectionContext } from './SelectionContext';
-import { getOffsetInTextContent } from './utils';
 
 const CommentableContainer = ({
   containerId,
@@ -12,8 +11,7 @@ const CommentableContainer = ({
 }) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const { setPositionedSelection, commentableContainers } =
-    useSelectionContext();
+  const { commentableContainers } = useSelectionContext();
 
   useEffect(() => {
     commentableContainers.current[containerId] = containerRef;
@@ -22,57 +20,8 @@ const CommentableContainer = ({
     };
   }, []);
 
-  const handleTextSelection = () => {
-    if (!containerRef.current) return;
-
-    const selection = window.getSelection();
-    if (!selection || selection.rangeCount == 0) {
-      setPositionedSelection(undefined);
-      return;
-    }
-
-    const range = selection.getRangeAt(0);
-
-    // Check if the selection is collapsed (i.e., it's just a single click)
-    if (range.collapsed) {
-      setPositionedSelection(undefined);
-      return;
-    }
-
-    // Check if both the start and end containers are within contentRef
-    if (
-      !containerRef.current.contains(range.startContainer) ||
-      !containerRef.current.contains(range.endContainer)
-    ) {
-      setPositionedSelection(undefined);
-      return;
-    }
-
-    const startOffset = getOffsetInTextContent(
-      containerRef.current,
-      range.startContainer,
-      range.startOffset,
-    );
-    const endOffset = getOffsetInTextContent(
-      containerRef.current,
-      range.endContainer,
-      range.endOffset,
-    );
-
-    const rect = range.getBoundingClientRect();
-    const scrollTop = document.documentElement.scrollTop;
-    const positionTop = rect.top + scrollTop;
-
-    setPositionedSelection({
-      startOffset,
-      endOffset,
-      containerId,
-      positionTop,
-    });
-  };
-
   return (
-    <div ref={containerRef} onMouseUp={handleTextSelection}>
+    <div data-container-id={containerId} ref={containerRef}>
       {children}
     </div>
   );
--- src/plugin/CommentableSection.tsx
+++ src/plugin/CommentableSection.tsx
@@ -4,7 +4,10 @@ import { ReactNode, useCallback, useEffect, useRef } from 'react';
 import NewCommentTrigger from './NewCommentTrigger';
 import { useSelectionContext } from './SelectionContext';
 import { CommentPositions } from './types';
-import { findNodeAndOffsetFromTotalOffset } from './utils';
+import {
+  findNodeAndOffsetFromTotalOffset,
+  getOffsetInTextContent,
+} from './utils';
 
 import './styles.css';
 
@@ -24,6 +27,7 @@ const CommentableSection = ({
     setCommentableSectionOffsetY,
     setCommentPositionState,
     setActiveCommentId,
+    setPositionedSelection,
   } = useSelectionContext();
 
   const setOffset = useCallback(() => {
@@ -95,23 +99,79 @@ const CommentableSection = ({
     };
   }, [reposition]);
 
-  const onClick = useCallback(
-    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      if (!(e.target instanceof HTMLElement)) return;
+  const handleTextSelection = () => {
+    if (!sectionRef.current) return;
 
-      if (!e.target.hasAttribute('data-comment-id')) return;
+    const selection = window.getSelection();
+    if (!selection || selection.rangeCount == 0) {
+      setPositionedSelection(undefined);
+      return;
+    }
 
-      const commentId = e.target.getAttribute('data-comment-id');
-      setActiveCommentId(commentId);
-    },
-    [setActiveCommentId],
-  );
+    const range = selection.getRangeAt(0);
+
+    // Check if the selection is collapsed (i.e., it's just a single click)
+    if (range.collapsed) {
+      setPositionedSelection(undefined);
+      return;
+    }
+
+    const container =
+      range.commonAncestorContainer.parentElement?.closest(
+        '[data-container-id]',
+      );
+
+    const containerId = container?.getAttribute('data-container-id');
+
+    const isWithinCommentable =
+      container && sectionRef.current.contains(container);
+
+    // Check if both the start and end containers are within contentRef
+    if (!containerId || !isWithinCommentable) {
+      setPositionedSelection(undefined);
+      return;
+    }
+
+    const startOffset = getOffsetInTextContent(
+      container,
+      range.startContainer,
+      range.startOffset,
+    );
+    const endOffset = getOffsetInTextContent(
+      container,
+      range.endContainer,
+      range.endOffset,
+    );
+
+    const rect = range.getBoundingClientRect();
+    const scrollTop = document.documentElement.scrollTop;
+    const positionTop = rect.top + scrollTop;
+
+    setPositionedSelection({
+      startOffset,
+      endOffset,
+      containerId,
+      positionTop,
+    });
+  };
+
+  const handleCommentSelection = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    if (!(e.target instanceof HTMLElement)) return;
+
+    if (!e.target.hasAttribute('data-comment-id')) return;
+
+    const commentId = e.target.getAttribute('data-comment-id');
+    setActiveCommentId(commentId);
+  };
 
   return (
     <div
       ref={sectionRef}
       style={{ position: 'relative' }}
-      onClick={onClick}
+      onMouseUp={handleTextSelection}
+      onClick={handleCommentSelection}
     >
       {children}
       <NewCommentTrigger>{addIcon}</NewCommentTrigger>
